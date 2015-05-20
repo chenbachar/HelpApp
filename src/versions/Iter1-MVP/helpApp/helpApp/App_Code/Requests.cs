da2e73b23ed997d99234421aed8a7b372eec2d66
matan-sh <matanshlomi88@gmail.com>
2015-05-20 18:38:09 +0300

    added the class Requests

---
+++ src/versions/Iter1-MVP/helpApp/helpApp/App_Code/Requests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace helpApp.App_Code
+{
+    public class Requests
+    {
+        private string requests;
+        private int phone;
+
+        public Requests(string requests, int phone)
+        {
+            this.requests = requests;
+            this.phone = phone;
+        }
+    }
+}
