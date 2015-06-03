b30ce8bf5b4001ddfd25c0978b23b4c3781ff4e6
Chen Bachar <henbachar2win@gmail.com>
2015-06-03 03:16:27 +0300

    small change in volunteer.js

--- src/versions/Iter1-MVP/helpapp-seproject/web/static/js/volunteer.js
+++ src/versions/Iter1-MVP/helpapp-seproject/web/static/js/volunteer.js
@@ -5,9 +5,11 @@ function getName(){
 function changeImg(){
 	var img=document.getElementById("imgStatus");
 	if(img.src.match("OFF")){
+		document.getElementById("status").innerHTML = "הסטטוס שלך: פעיל";
 		img.src="/static/img/ON.jpg";
 	}
 	else{
+		document.getElementById("status").innerHTML = "הסטטוס שלך: לא פעיל";
 		img.src="/static/img/OFF.jpg";
 	}
 }
