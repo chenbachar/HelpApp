853afef87f89a17e0be0172162b9bc689451f8a0
Matan Frank <mtnfrnk@gmail.com>
2015-06-28 13:37:33 +0300

    ANDPROID IMPROVMENT

--- src/versions/AndroidCode/helpAPP/app/src/main/java/com/appspot/helpapp_seproject/helpapp/MainActivity.java
+++ src/versions/AndroidCode/helpAPP/app/src/main/java/com/appspot/helpapp_seproject/helpapp/MainActivity.java
@@ -1,30 +1,55 @@
 package com.appspot.helpapp_seproject.helpapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity
+{
     private WebView myWebView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
+        String host = "https://accounts.google.com/ServiceLogin?service=ah&passive=true&continue=https%3A%2F%2Fappengine.google.com%2F_ah%2Fconflogin%3Fcontinue%3Dhttp%3A%2F%2Fhelpapp-seproject.appspot.com%2Fvolunteer&ltmpl=gm&shdf=Ch0LEgZhaG5hbWUaEWhlbHBhcHAtc2Vwcm9qZWN0DBICYWgiFEK4pWOvEQU3Mj5EzWNEwt5VZdp_KAEyFHuf-cjnHl2y_M40zx1JNHNe6da-";
+        String realm = "helpApp";
+        //in case of notification entry
+        String urlStr = "http://helpapp-seproject.appspot.com/";
+        Intent intent = getIntent();
+        String userDirect = intent.getStringExtra("afterNtf");//going to direction "helpapp/.../connect"
+        if(!(userDirect == null))
+          urlStr = urlStr + userDirect;
+
+        //open the website
         setContentView(R.layout.activity_main);
         myWebView = (WebView) findViewById(R.id.webView);
-        myWebView.loadUrl("http://helpapp-seproject.appspot.com/");
+        myWebView.loadUrl(urlStr);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.setWebViewClient(new WebViewClient());
+
+
+        String[] result = instance.getHttpAuthUsernamePassword(host, realm);
+
+
+        //open the background thread
+        Intent i = new Intent(this, popUpMessage.class);
+        System.out.println(urlStr + " 1234");
+        i.setData(Uri.parse(urlStr));
+        startService(i);
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
         // Check if the key event was the Back button and if there's history
         if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
             myWebView.goBack();
@@ -33,27 +58,5 @@ public class MainActivity extends ActionBarActivity {
         // If it wasn't the Back key or there's no web page history, bubble up to the default
         // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- src/versions/AndroidCode/helpAPP/app/src/main/java/com/appspot/helpapp_seproject/helpapp/popUpMessage.java
+++ src/versions/AndroidCode/helpAPP/app/src/main/java/com/appspot/helpapp_seproject/helpapp/popUpMessage.java
@@ -10,12 +10,28 @@ import android.util.Log;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.client.AbstractHttpClient;
+import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.cookie.Cookie;
+
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.CookieStore;
+import java.util.List;
+
+import java.util.ArrayList;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
 
 public class popUpMessage extends IntentService
 {
@@ -29,7 +45,7 @@ public class popUpMessage extends IntentService
     {
         InputStream ins = null;
         HttpClient httpC;
-        HttpPost httpP;
+        HttpGet httpP;
         HttpResponse httpR;
         HttpEntity httpE;
         boolean ntfFlag = false;
@@ -45,11 +61,40 @@ public class popUpMessage extends IntentService
             try//connect to the server
             {
                 httpC = new DefaultHttpClient();
-                //httpP = new HttpPost("http://ephemeraltech.com/demo/android_tutorial20.php");
-                httpP = new HttpPost("http://helpapp-seproject.appspot.com/get_notification");
+//                httpP = new HttpPost("http://ephemeraltech.com/demo/android_tutorial20.php");
+                httpP = new HttpGet("http://helpapp-seproject.appspot.com/get_notification");
                 httpR = httpC.execute(httpP);
                 httpE = httpR.getEntity();
                 ins = httpE.getContent();
+
+
+
+                // Create a local instance of cookie store
+                CookieStore cookieStore = (CookieStore) new BasicCookieStore();
+
+                // Create local HTTP context
+                HttpContext localContext = new BasicHttpContext();
+                // Bind custom cookie store to the local context
+                localContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
+
+                HttpGet httpget = new HttpGet("http://www.google.com/", localContext);
+
+
+
+                List<Cookie> cookies = ((AbstractHttpClient) httpC).cookieStore().getCookies();
+                //List<Cookie> cookies = httpC.getCookieStore().getCookies();
+                if (cookies.isEmpty()) {
+                    System.out.println("None");
+                } else {
+                    for (int i = 0; i < cookies.size(); i++) {
+                        System.out.println("- " + cookies.get(i).toString());
+                    }
+                }
+
+                HttpPost httpost = new HttpPost("https://portal.sun.com/amserver/UI/Login?" +
+                        "org=self_registered_users&" +
+                        "goto=/portal/dt&" +
+                        "gotoOnFail=/portal/dt?error=true");
             }
             catch (Exception e)
             {
@@ -59,11 +104,16 @@ public class popUpMessage extends IntentService
             try //convert httpR to string
             {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(ins, "iso-8859-1"));
+//                StringBuilder sb =new StringBuilder();
+//                String line = null;
+//                while((line = reader.readLine()) != null)
+//                    sb.append(line + "\n");
+//                String helpRequest = sb.toString();
                 String helpRequest = reader.readLine();
                 ins.close();
-                System.out.println("helpRequest: " + helpRequest+ " 1234");
-//              if(helpRequest.equals("\"TUTORIAL 20 WORKED, WE GOT CONNECTION\" "))
-                if(helpRequest.equals("true"))
+                System.out.println("helpRequest:" + helpRequest+ "1234");
+//                if(helpRequest.contains("\"TUTORIAL 20 WORKED, WE GOT CONNECTION\" "))
+                if(helpRequest.contains("True"))
                     ntfFlag = true;
             }
             catch (Exception e)
@@ -79,11 +129,12 @@ public class popUpMessage extends IntentService
         if (intent != null)
         {
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
-                            .setSmallIcon(R.drawable.ic_launcher)
-                            .setContentTitle("HelpApp")
-                            .setContentText("You got a new notification");
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    .setContentTitle("HelpApp")
+                    .setContentText("You got a new notification");
 
-            Intent resultIntent = new Intent(this, notificationClick.class);
+            Intent resultIntent = new Intent(this, MainActivity.class);
+            resultIntent.putExtra("afterNtf", "connect");
 // Because clicking the notification opens a new ("special") activity, there's
 // no need to create an artificial back stack.
             PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
